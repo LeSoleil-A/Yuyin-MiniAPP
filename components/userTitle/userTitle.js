e46cb6f1146e46cfb6ac7d3618f97558cd48982e
Fantast416 <45848260+Fantast416@users.noreply.github.com>
2021-02-15 20:57:42 +0800

    新修改userTitle组件
    增加获取用户信息

--- components/userTitle/userTitle.js
+++ components/userTitle/userTitle.js
@@ -2,11 +2,41 @@ Component({
   mixins: [],
   data: {},
   props: {
-    userName: "",
-    userImage: ""
+    canIUse: false, //用户之前有没有授权过
+    nickName: "",
+    userImageSrc: ""
   },
-  didMount() {},
-  didUpdate() {},
-  didUnmount() {},
-  methods: {},
+  didMount() {
+    this.authorize();
+  },
+  didUpdate() { },
+  didUnmount() { },
+
+  methods: {
+    authorize() {
+      var that = this
+      my.getOpenUserInfo({
+        fail: (res) => {
+          console.log(res)
+        },
+        success: (res) => {
+          let userInfo = JSON.parse(res.response).response // 以下方的报文格式解析两层 response
+          if (userInfo.code != "10000") {
+            that.setData({
+              canIUse: false
+            })
+          } else {
+            that.setData({
+              nickName: userInfo.nickName,
+              userImageSrc: userInfo.avatar,
+              canIUse: true
+            })
+          }
+        },
+      });
+    },
+    onGetAuthorize() {
+      this.authorize()
+    },
+  },
 });
--- pages/MinePage/MinePage.js
+++ pages/MinePage/MinePage.js
@@ -1,46 +1,47 @@
 Page({
   data: {
+    canIUseAuthButton: true,
     medalIcon: "/pages/MinePage/image/medal_test.png",
     array: [{
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
-    },{
+    }, {
       src: "XNDM0OTQzMDc2OA==",
     }, {
       src: "XNDM0OTQzMDc2OA==",
     },
     ],
   },
-  onLoad() {},
+  onLoad() { },
   lightMore() {
     console.log("Tap on lightMore!")
   }
