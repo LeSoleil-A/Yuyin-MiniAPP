257832098a0ae9c473b50d98fa06752f414314f7
Fantast416 <45848260+Fantast416@users.noreply.github.com>
2021-02-21 12:13:22 +0800

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  api_base_url: 'http://127.0.0.1:8000/', // 这是本地调试的api地址
+  api_base_url: 'http://8.136.99.25:7000/', // 这是远程调试的api地址
   ali_oss: {
     host: 'https://yy-yuyin.oss-cn-hangzhou.aliyuncs.com',
     ossAccessKeyId: 'LTAI4G5RkGzsEjHHLY2dk5xF'
