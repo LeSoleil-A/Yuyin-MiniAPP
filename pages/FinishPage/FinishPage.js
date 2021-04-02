86cb17dbe605ce3ce922978477f071168f909b81
Michaek Asama Luk <52308484+LuniumLuk@users.noreply.github.com>
2021-04-02 22:54:16 +0800

    Add Camera Func

--- pages/FinishPage/FinishPage.js
+++ pages/FinishPage/FinishPage.js
@@ -1,11 +1,30 @@
 Page({
   data: {
-    venueId: ""
+    statusBarHeight: 0,
+    titleBarHeight: 0,
+    venueId: ''
   },
   onLoad(query) {
-    console.log("得到的venue_id: " + query.venueId);
+    // 获取camera和video组件
+    this.cameraCtx = my.createCameraContext('camera')
+    this.videoCtx = my.createVideoContext('video')
+    // 获取屏幕高度信息
+    my.getSystemInfo({
+      success: res => {
+        this.setData({
+          statusBarHeight: res.statusBarHeight,
+          titleBarHeight: res.titleBarHeight,
+        })
+      }
+    })
+    console.log('venueId: ', query.venueId)
     this.setData({
-      venueId: query.venueId,
-    });
+      venueId: query.venueId
+    })
   },
-});
+  gotoCamera() {
+    my.navigateTo({
+      url: `/pages/CameraPage/CameraPage?venueId=${ this.data.venueId }`
+    })
+  }
+})
